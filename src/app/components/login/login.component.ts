82513d796cab74640ce9beb6e15d776b08438aec
VitalyZibulski <vitaly.zibulski@gmail.com>
2018-05-20 09:47:04 +0300

    frontend refactor with service

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -16,7 +16,7 @@ export class LoginComponent implements OnInit {
 
     public error = null;
 
-    constructor(private Jarwis:JarwisService) {
+    constructor(private Jarwis:JarwisServicee) {
     }
 
     onSubmit() {
